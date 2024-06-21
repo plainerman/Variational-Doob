233de6b2dd28ff755cb6296cbc2b2fd283763cc1
Michael Plainer <6825443+plainerman@users.noreply.github.com>
2024-06-21 15:25:44 +0200

    Allow argparse to handle nargs

--- utils/args.py
+++ utils/args.py
@@ -4,7 +4,6 @@ import yaml
 
 
 def parse_args(parser: ArgumentParser):
-    # args = parser.parse_args()
     args = sys.argv[1:]
     conf = args.index('--config')
     if conf >= 0 and conf + 1 < len(args):
@@ -19,21 +18,9 @@ def parse_args(parser: ArgumentParser):
     if config is None:
         raise ValueError(f"Config file {config_file} is empty")
 
-    cli_args = {}
-    # convert args to dictionary
-    i = 0
-    while i < len(args):
-        if args[i].startswith('--'):
-            cli_args[args[i].replace('--', '')] = args[i + 1]
-            i += 2
-        else:
-            i += 1
-
-    config |= cli_args
-
     config_args = []
     for k, v in config.items():
         config_args.append(f'--{k}')
         config_args.append(str(v))
 
-    return parser.parse_args(args=config_args)
+    return parser.parse_args(args=config_args + args)
